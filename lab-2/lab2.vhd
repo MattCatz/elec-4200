d56a50e50083aa34fd798467fb1bfb11f3adb040
Matthew <14895427+MattCatz@users.noreply.github.com>
2018-09-08 12:59:10 -0700

    Got the testbed working, problems with timing

--- lab-2/lab2.vhd
+++ lab-2/lab2.vhd
@@ -16,32 +16,35 @@ use IEEE.STD_LOGIC_1164.ALL;
 -- use UNISIM.VComponents.all;
 
 entity Moore_FSM_Equations_FD is
-    Port ( CLK : in STD_LOGIC;--Clock
-           RST : in STD_LOGIC;--Active Low synchcronus reset
-           PB : in STD_LOGIC;--Active High clock enable from pushbutton
-           Cout : out STD_LOGIC_VECTOR (1 downto 0);--Binary representation fo the current state
-           Oout : out STD_LOGIC_VECTOR (3 downto 0));--One-cold representation fo the current state
+    Port ( CLK : in STD_LOGIC; --Clock
+           RST : in STD_LOGIC; --Active Low synchcronus reset
+           PB : in STD_LOGIC;  --Active High clock enable from pushbutton
+           Cout : out STD_LOGIC_VECTOR (1 downto 0);  --current state
+           Oout : out STD_LOGIC_VECTOR (3 downto 0)); --position  state
 end Moore_FSM_Equations_FD;
 
 architecture Behavioral of Moore_FSM_Equations_FD is
 ---------------------Begin template signals----------------------------
-signal C : STD_LOGIC_VECTOR(1 downto 0);  --Internal signal for output C, which is a binary representation of the current state
-signal O : STD_LOGIC_VECTOR (3 downto 0); --Internal signal for output O, which is a one-cold representation of the current state
+signal C : STD_LOGIC_VECTOR(1 downto 0) :="00";  --Internal signal for output C, which is a binary representation of the current state
+signal O : STD_LOGIC_VECTOR (3 downto 0) :="0000"; --Internal signal for output O, which is a one-cold representation of the current state
 signal X,Y,Z,EN: STD_LOGIC; --Signals for the digital one-shot and enable
 ---------------------End template signals------------------------------
 
 --Create any signals you need here (for example, you will need a signal to hold the next state logic for your flipflops)
+
+signal jumper : STD_LOGIC := '0'; --Used to jump start the counter
+signal Q : STD_LOGIC_VECTOR(3 downto 0) := "0001"; --Internal signals for Q flip-flop outputs
 
 begin
 
 -- Template for D flip-flop model
 -- Copy and paste for each D flip-flop instance
 -- Each instance must have a unique Instance_Label
--- Substitute actual signal names for clock-signal, Q-output, D-input
+-- Substitute actual signal names for CLK, Q-output, D-input
 --
---Instance_Label :  process (clock-signal)
+--Instance_Label :  process (CLK)
 --                  begin
---                      if rising_edge(clock-signal) then
+--                      if rising_edge(CLK) then
 --                            Q-output <= D-input;
 --                      end if;
 --                  end process;
@@ -97,5 +100,44 @@ Oout <= O;
 --The data input should be supplied by your next state logic.
 --The data output will be used in the next state logic and output logic.
 --You will need to assign values C and O.
+
+--Begin ring-counter logic
+FF_0 :  process (CLK)
+  begin
+    if rising_edge(CLK) then        --I do not think I need to check clock
+      Q(0) <= jumper and not RST and PB; --because rising_edge only returns
+    end if;                                  --true on the 0->1 transition
+  end process;
+
+FF_1 :  process (CLK)
+  begin
+    if rising_edge(CLK) then
+      Q(1) <= Q(0) and not RST and PB;
+    end if;
+  end process;
+
+FF_2 :  process (CLK)
+  begin
+    if rising_edge(CLK) then
+      Q(2) <= Q(1) and not RST and PB;
+    end if;
+  end process;
+
+FF_3 :  process (CLK)
+  begin
+    if rising_edge(CLK) then
+      Q(3) <= Q(2) and not RST and PB;
+    end if;
+  end process;
+
+jumper <= not Q(3) and not Q(2) and not Q(1); --This could probably be generalized
+                                  --like mention in lecture the other day
+--End ring-counter logic
+
+--Begin output logic
+O <= not Q;
+C(0) <= Q(2) and not Q(0);
+C(1) <= Q(2) and not Q(1);
+--End output logic
 
 end Behavioral;
---
+++ lab-2/ring_counter_tb.vhd
@@ -0,0 +1,73 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+library work;
+use work.txt_util.all;
+
+entity ring_counter_tb is
+end entity;
+
+architecture testbench of ring_counter_tb is 
+
+  component Moore_FSM_Equations_FD is
+    port (
+           CLK : in STD_LOGIC;
+           RST : in STD_LOGIC;
+           PB : in STD_LOGIC;
+           Cout : out STD_LOGIC_VECTOR (1 downto 0);
+           Oout : out STD_LOGIC_VECTOR (3 downto 0)
+    );
+  end component;
+
+  for ring_counter: Moore_FSM_Equations_FD use entity work.Moore_FSM_Equations_FD;
+  signal CLK : STD_LOGIC := '0';
+  signal FIN : STD_LOGIC := '0';
+  signal RST, PB : STD_LOGIC;
+  signal C : STD_LOGIC_VECTOR (1 downto 0);
+  signal O : STD_LOGIC_VECTOR (3 downto 0);
+  constant half_period : TIME := 5 ms;
+  
+  begin
+    ring_counter: Moore_FSM_Equations_FD port map (CLK => CLK,
+                                                   RST => RST,
+                                                   PB  => PB,
+                                                   Cout   => C,
+                                                   Oout   => O);
+  gen_clk : process (CLK)
+  begin
+      --assert false report "clock" severity note;
+      CLK <= not CLK after half_period;
+  end process;
+
+  process
+    type test_vector is record
+       RST, PB : STD_LOGIC;
+       O : STD_LOGIC_VECTOR (3 downto 0);
+       C : STD_LOGIC_VECTOR (1 downto 0);
+    end record;
+
+    type test_vector_array is array (natural range <>) of test_vector;
+    constant test_vectors : test_vector_array := (
+      -- RST    PB   O       C
+         ('0', '0',  "1110", "00"), --Check init state
+         ('0', '1',  "1101", "01"), --A->B
+         ('0', '1',  "1011", "10"), --B->C
+         ('0', '1',  "0111", "11"), --C->D
+         ('0', '1',  "1111", "00")  --D->A
+    );
+    begin
+      report "Starting Loop" severity note;
+      for i in test_vectors'range loop
+	report "Vector: " & str(i);
+        RST <= test_vectors(i).RST; 
+        PB  <= test_vectors(i).PB;
+        wait until rising_edge(CLK);
+        assert O = test_vectors(i).O report "Failed O:" & str(test_vectors(i).O) & "->" & str(O); 
+        assert C = test_vectors(i).C report "Failed C:" & str(C);
+      end loop;
+      report "end of test" severity note;
+      FIN <= '1';
+      wait;
+  end process;
+
+end testbench;
