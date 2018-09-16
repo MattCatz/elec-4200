c93378a6282156d92c069c7219667264c5216733
Matthew <14895427+MattCatz@users.noreply.github.com>
2018-09-16 16:05:47 -0700

    Finished draft for lab 4

---
+++ lab-4/Moore_FSM_Equations_FD.vhd
@@ -0,0 +1,119 @@
+----------------------------------------------------------------------------------
+-- Engineer: Matthew Cather
+--
+-- Create Date: 9/16/18
+-- Module Name: Moore_FSM_Equations_FD
+-- Project Name: Sequential Logic Design
+--
+----------------------------------------------------------------------------------
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity Moore_FSM_Equations_FD is
+    Port ( CLK : in STD_LOGIC; --Clock
+           RST : in STD_LOGIC; --Active Low synchcronus reset
+           PB : in STD_LOGIC;  --Active High clock enable from pushbutton
+           Cout : out STD_LOGIC_VECTOR (1 downto 0);  --current state
+           Oout : out STD_LOGIC_VECTOR (3 downto 0)); --position  state
+end Moore_FSM_Equations_FD;
+
+architecture Behavioral of Moore_FSM_Equations_FD is
+---------------------Begin template signals----------------------------
+signal C : STD_LOGIC_VECTOR(1 downto 0) :="00";  --Internal signal for output C, which is a binary representation of the current state
+signal O : STD_LOGIC_VECTOR (3 downto 0) :="1110"; --Internal signal for output O, which is a one-cold representation of the current state
+signal X,Y,Z,EN: STD_LOGIC; --Signals for the digital one-shot and enable
+---------------------End template signals------------------------------
+begin
+
+-- Template for D flip-flop model
+-- Copy and paste for each D flip-flop instance
+-- Each instance must have a unique Instance_Label
+-- Substitute actual signal names for CLK, Q-output, D-input
+--
+--Instance_Label :  process (CLK)
+--                  begin
+--                      if rising_edge(CLK) then
+--                            Q-output <= D-input;
+--                      end if;
+--                  end process;
+
+
+---------------Begin digital one-shot model---------------------------
+---------------Study, but do not edit!--------------------------------
+
+-- First D flip-flop of the one-shot circuit
+ONESHOTFF1 : process (CLK)
+  begin
+    if rising_edge(CLK) then  -- trigger on rising clock edge
+      X <= PB;                -- PB = D-input, X = Q-output
+    end if;
+  end process;
+
+-- Second D flip-flop of the one-shot circuit
+ONESHOTFF2 : process (CLK)
+  begin
+    if rising_edge(CLK) then  -- trigger on rising clock edge
+      Y <= X;                 -- X = D-input, Y = Q-output
+    end if;
+  end process;
+
+-- Third D flip-flop of the one-shot circuit
+ONESHOTFF3 : process (CLK)
+  begin
+    if rising_edge(CLK) then  -- trigger on rising clock edge
+      Z <= Y;                 -- Y = D-input, Z = Q-output
+    end if;
+  end process;
+
+--Create enable signal with the output of the oneshot
+--Disabled for testing
+--EN <= Y and not Z;
+EN <= PB;
+
+---------------------End digital one-shot code---------------------
+
+Cout <= C;
+Oout <= O;
+---------------------Enter your code below-------------------------
+
+process (CLK) is
+begin
+  if rising_edge(CLK) then
+    case C is
+      when "00" => -- A
+        if RST = '1' then -- Goto A
+          C <= "00";
+          O <= "1110";
+        elsif EN = '1' then -- Goto B
+          C <= "01";
+          O <= "1101";
+        end if;
+      when "01" => -- B
+        if RST = '1' then -- Goto A
+          C <= "00";
+          O <= "1110";
+        elsif EN = '1' then -- Goto C
+          C <= "10";
+          O <= "1011";
+        end if;
+      when "10" => -- C
+        if RST = '1' then -- Goto A
+          C <= "00";
+          O <= "1110";
+        elsif EN = '1' then -- Goto D
+          C <= "11";
+          O <= "0111";
+        end if;
+      when "11" => -- D
+        if (RST='1') OR (EN='1') then -- Goto A
+          C <= "00";
+          O <= "1110";
+        end if;
+      when others =>
+          C <= "00";
+          O <= "1110";
+    end case;
+  end if;
+end process;
+end Behavioral;
---
+++ lab-4/Moore_FSM_Equations_FD_tb.vhd
@@ -0,0 +1,101 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+library work;
+use work.txt_util.all;
+
+entity Moore_FSM_Equations_FD_tb is
+end entity;
+
+architecture testbench of Moore_FSM_Equations_FD_tb is
+
+  component Moore_FSM_Equations_FD is
+    port (
+           CLK : in STD_LOGIC;
+           RST : in STD_LOGIC;
+           PB : in STD_LOGIC;
+           Cout : out STD_LOGIC_VECTOR (1 downto 0);
+           Oout : out STD_LOGIC_VECTOR (3 downto 0)
+    );
+  end component;
+
+  for ring_counter: Moore_FSM_Equations_FD use entity work.Moore_FSM_Equations_FD;
+  signal CLK : STD_LOGIC := '0';
+  signal FIN : STD_LOGIC := '0';
+  signal RST, PB : STD_LOGIC;
+  signal C : STD_LOGIC_VECTOR (1 downto 0);
+  signal O : STD_LOGIC_VECTOR (3 downto 0);
+  constant half_period : TIME := 5 ns;
+  
+  begin
+    ring_counter: Moore_FSM_Equations_FD port map (CLK => CLK,
+                                                   RST => RST,
+                                                   PB  => PB,
+                                                   Cout   => C,
+                                                   Oout   => O);
+  gen_clk : process (CLK)
+  begin
+      if FIN = '0' then
+        CLK <= not CLK after half_period;
+      else
+	CLK <= '0';
+      end if;
+  end process;
+
+  process
+    type test_vector is record
+       RST, PB : STD_LOGIC;
+       O : STD_LOGIC_VECTOR (3 downto 0);
+       C : STD_LOGIC_VECTOR (1 downto 0);
+    end record;
+
+    type test_vector_array is array (natural range <>) of test_vector;
+    constant test_vectors : test_vector_array := (
+      -- RST    PB   O       C
+         ('0', '0',  "1110", "00"), --Check init state
+         ('0', '1',  "1101", "01"), --A->B
+         ('0', '1',  "1011", "10"), --B->C
+         ('0', '1',  "0111", "11"), --C->D
+         ('0', '1',  "1110", "00"), --D->A
+         ('1', '1',  "1110", "00"), --A->A
+         ('0', '1',  "1101", "01"), --A->B
+         ('1', '1',  "1110", "00"), --B->A
+         ('0', '1',  "1101", "01"), --A->B
+         ('0', '1',  "1011", "10"), --B->C
+         ('1', '1',  "1110", "00"),  --C->A
+         ('0', '1',  "1101", "01"), --A->B
+         ('0', '1',  "1011", "10"), --B->C
+         ('0', '1',  "0111", "11"), --C->D
+         ('1', '1',  "1110", "00"), --D->A
+         ('1', '0',  "1110", "00"), --A->A
+         ('0', '1',  "1101", "01"), --A->B
+         ('1', '0',  "1110", "00"), --B->A
+         ('0', '1',  "1101", "01"), --A->B
+         ('0', '1',  "1011", "10"), --B->C
+         ('1', '0',  "1110", "00"), --C->A
+         ('0', '1',  "1101", "01"), --A->B
+         ('0', '1',  "1011", "10"), --B->C
+         ('0', '1',  "0111", "11"), --C->D
+         ('1', '0',  "1110", "00") --D->A
+    );
+    begin
+      report "Starting tests" severity note;
+      for i in test_vectors'range loop
+        report "Vector: " & str(i);
+        RST <= test_vectors(i).RST;
+        PB  <= test_vectors(i).PB;
+        wait until rising_edge(CLK);
+        wait for 1 ns;
+        RST <= '0';
+        PB <= '0';
+        wait until rising_edge(CLK); -- check that we can stay here
+        wait for 1 ns; -- transition time
+        assert O = test_vectors(i).O report "Failed O:" & str(test_vectors(i).O) & "->" & str(O);
+        assert C = test_vectors(i).C report "Failed C:" & str(test_vectors(i).C) & "->" & str(C);
+      end loop;
+      report "End of tests" severity note;
+      FIN <= '1';
+      wait;
+  end process;
+
+end testbench;
