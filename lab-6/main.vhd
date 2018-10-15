69060fbf49db13946e5b7bbc6619225357875382
Matthew Cather <14895427+MattCatz@users.noreply.github.com>
2018-10-15 17:30:19 -0500

    Update main.vhd

--- lab-6/main.vhd
+++ lab-6/main.vhd
@@ -12,7 +12,8 @@ entity Lab_6_TOP is
         r_addr: in std_logic_vector(M-1 downto 0);
         w_addr : in std_logic_vector(M-1 downto 0);
         di : in std_logic_vector(N-1 downto 0);
-        do : out std_logic_vector(N-1 downto 0));
+        do : out std_logic_vector(N-1 downto 0);
+        CLK: in std_logic);
 end entity Lab_6_TOP;
 
 architecture Hierarchy of Lab_6_TOP is
@@ -20,12 +21,12 @@ architecture Hierarchy of Lab_6_TOP is
 
 	begin
 
-	DB: WORK.Debounce port map
-			(CLK=>'1', PB=>PB, PBdb=>PBdb);
+	DB: entity WORK.Debounce port map
+			(CLK=>CLK, PB=>PB, PBdb=>PBdb);
 
-	One_Shot: WORK.One_Shot_Timer port map
-			(CLK=>'1', PB=>PBdb, EN=>EN);
+	One_Shot: entity  WORK.One_Shot_Timer port map
+			(CLK=>CLK, PB=>PBdb, EN=>EN);
 
-	memory: WORK.Memory generic map (M,N) port map
+	memory: entity  WORK.Memory generic map (M,N) port map
 			(WE=>EN, r_addr=>r_addr, w_addr=>w_addr, di=>di, do=>do);
 end architecture Hierarchy;
