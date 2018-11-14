2453efc813df0e173d3fae83d3f59e9dc913d0a3
Matthew Cather <14895427+MattCatz@users.noreply.github.com>
2018-11-14 15:03:21 -0600

    Update spiReceiver_tb.vhd

--- lab-10/spiReceiver_tb.vhd
+++ lab-10/spiReceiver_tb.vhd
@@ -11,14 +11,14 @@ end entity Lab_10_TOP_tb;
 architecture testbench of Lab_10_TOP_TB is
     constant half_period : TIME := 5 ns;
     
-    signal MOSI,SCK,ACK,CLK,RDY,RST: std_logic := '0';
+    signal MOSI,SCK,ACK,CLK,RDY: std_logic := '0';
 
     signal OUTPUT: std_logic_vector(5 downto 0);
     
     signal FIN : std_logic := '0';
 
 	begin
-	receiver : entity WORK.spi_receiver port map(MOSI=>MOSI,SCK=>SCK,ACK=>ACK,CLK=>CLK,RDY=>RDY,OUTPUT=>OUTPUT,RST=>RST);
+	receiver : entity WORK.spi_receiver port map(MOSI=>MOSI,SCK=>SCK,ACK=>ACK,CLK=>CLK,RDY=>RDY,OUTPUT=>OUTPUT);
 
 	test_clk : process (CLK)
 	begin
@@ -31,41 +31,58 @@ architecture testbench of Lab_10_TOP_TB is
 
 	process
 		type test_vector is record
-			RST,MOSI,SCK,ACK,RDY : STD_LOGIC;	
+			MOSI,SCK,ACK,RDY : STD_LOGIC;	
 			Q : Integer;
 		end record;
 
 		type test_vector_array is array (natural range <>) of test_vector;
 		constant test_vectors : test_vector_array := (
-		--  RST MOSI, SCK, ACK, RDY    Q
-			('1', '0', '0', '0', '0',  0), -- Inital State
-			('0', '1', '1', '0', '0',  0), -- Send one
-			('0', '1', '1', '0', '0',  0), -- Send one
-			('0', '0', '1', '0', '0',  0), -- Send zero
-			('0', '1', '1', '0', '0',  0), -- Send one
-			('0', '0', '1', '0', '0',  0), -- Send zero
-			('0', '0', '1', '0', '1', 11), -- Send zero and expect the results to be ready
-			('0', '0', '0', '1', '0', 11), -- Acknowledge the results
-			('0', '0', '1', '0', '0', 11), -- Send zero
-			('0', '0', '1', '0', '0', 11), -- Send zero
-			('0', '0', '1', '0', '0', 11), -- Send zero
-			('0', '0', '1', '0', '0', 11), -- Send zero
-			('0', '0', '1', '0', '0', 11), -- Send zero
-			('0', '0', '1', '0', '1',  0), -- Send zero and expect the results to be ready
-			('0', '0', '0', '1', '0',  0), -- Acknowledge the results
-			('0', '1', '0', '0', '0',  0), -- Signal but dont send
-			('0', '1', '0', '0', '0',  0), -- Signal but dont send
-			('0', '1', '0', '0', '0',  0), -- Signal but dont send
-			('0', '1', '0', '0', '0',  0), -- Signal but dont send
-			('0', '1', '0', '0', '0',  0), -- Signal but dont send
-			('0', '1', '0', '0', '0',  0), -- Signal but dont send
-			('0', '0', '0', '0', '0',  0), -- Expect no results
-			('1', '1', '1', '0', '0',  0), -- Send one
-			('1', '1', '1', '0', '0',  0), -- Send one
-			('1', '1', '1', '0', '0',  0), -- Send one
-			('1', '1', '1', '0', '0',  0), -- Send one
-			('1', '1', '1', '0', '0',  0), -- Send one
-            ('0', '1', '1', '1', '0', 63)); -- Send one and Acknowledge early
+		-- MOSI, SCK, ACK, RDY    Q
+			('0', '0', '0', '0',  0), -- Inital State
+			('1', '1', '0', '0',  0), -- Send one
+			('1', '1', '0', '0',  0), -- Send one
+			('0', '1', '0', '0',  0), -- Send zero
+			('1', '1', '0', '0',  0), -- Send one
+			('0', '1', '0', '0',  0), -- Send zero
+			('0', '1', '0', '0', 52), -- Send zero and expect the results to be ready
+			('0', '0', '0', '1', 52), -- Wait to acknowledge
+			('0', '0', '1', '0', 52), -- Acknowledge the results
+			('0', '1', '0', '0', 52), -- Send zero
+			('0', '1', '0', '0', 52), -- Send zero
+			('0', '1', '0', '0', 52), -- Send zero
+			('0', '1', '0', '0', 52), -- Send zero
+			('0', '1', '0', '0', 52), -- Send zero
+			('0', '1', '0', '0',  0), -- Send zero
+			('0', '0', '0', '1',  0), -- Wait to acknowledge
+			('0', '0', '1', '0',  0), -- Acknowledge the results
+			('1', '0', '0', '0',  0), -- Signal but dont send
+			('1', '0', '0', '0',  0), -- Signal but dont send
+			('1', '0', '0', '0',  0), -- Signal but dont send
+			('1', '0', '0', '0',  0), -- Signal but dont send
+			('1', '0', '0', '0',  0), -- Signal but dont send
+			('1', '0', '0', '0',  0), -- Signal but dont send
+			('0', '0', '0', '0',  0), -- Expect no results
+			('1', '1', '0', '0',  0), -- Send one
+			('1', '1', '0', '0',  0), -- Send one
+			('1', '1', '0', '0',  0), -- Send one
+			('1', '1', '0', '0',  0), -- Send one
+			('1', '1', '0', '0',  0), -- Send one
+            ('1', '1', '0', '0', 63), -- Send one
+            ('0', '0', '0', '1', 63), -- Wait to acknowledge
+            ('0', '1', '1', '0', 63), -- Send zero
+            ('0', '1', '0', '0', 63), -- Send zero
+            ('1', '1', '0', '0', 63), -- Send one
+            ('1', '1', '0', '0', 63), -- Send one
+            ('0', '1', '0', '0', 63), -- Send zero
+            ('1', '1', '0', '0', 13), -- Send one and expect the results to be ready
+            ('0', '0', '0', '1', 13), -- Wait to acknowledge
+            ('0', '0', '1', '0', 13), -- expect to only have to do this once
+            ('0', '0', '0', '0', 13), -- wait
+            ('0', '0', '0', '0', 13), -- wait
+            ('0', '0', '0', '0', 13), -- wait
+            ('0', '0', '0', '0', 13), -- wait
+            ('0', '0', '0', '0', 13), -- wait
+            ('0', '0', '0', '0', 13)); -- wait
 		begin
 			report "Starting Tests" severity note;
 			for i in test_vectors'range loop
